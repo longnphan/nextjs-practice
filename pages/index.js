7b5cff212ac227dd130bb167a7b08f428a97e4bd
Long Phan <longphan@Longs-Laptop.local>
2022-11-25 22:55:51 -0700

    add 404.js, about.js, portfolio folder

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 const HomePage = () => {
     return (
-        <>
-            <div>
-                <h1>The Home Page</h1>
-            </div>
-        </>
+        <div>
+            <h1>The Home Page</h1>
+            <ul>
+                <li>
+                    <Link href="/portfolio">Portfolio</Link>
+                </li>
+            </ul>
+        </div>
+
     )
 }
 
